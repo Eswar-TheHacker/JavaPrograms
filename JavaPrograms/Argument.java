4808dfde1b7c69ab09ce74e6439b90fa3f675db7
eswarlella <eswar.lella1995@gmail.com>
2026-02-18 01:22:35 +0530

    Argument committing

---
+++ JavaPrograms/Argument.java
@@ -0,0 +1,8 @@
+package JavaPrograms;
+
+public class Argument {
+    public static void main(String[] args) {
+        System.out.println("The first argument is: " + args);
+        System.out.println("The second argument is: " + args);
+    
+}
